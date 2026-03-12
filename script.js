6075875a8674d9692d9b987ce8c73255d8155a1b
CamilaSilva73 <csilvadonascimento73@gmail.com>
2026-03-12 16:53:09 -0500

    subindo atualização

---
+++ script.js
@@ -0,0 +1,58 @@
+const lista = document
+
+const cafe = [
+
+    {
+nome: "Espresso",
+descricao: "Café concentrado y aromático",
+preco: "$3.50",
+Img: "assets/foto cafe espresso.jpg"
+    },
+
+{
+nome: "Cappuccino",
+descricao: "Espresso con leche vaporizada y espuma",
+preco: "$4.50",
+img: "assets/foto_cappuccino.jpg"
+},
+ 
+{
+nome: "Café Helado",
+descricao: "Refrescante café frío con hielo",
+preco: "$5.00",
+img: "assets/foto Café Helado.jpg"
+}
+]
+
+const resposteria = [
+
+    {
+nome: "Pastel de Chocolate",
+descricao: "Delicioso pastel de chocolate casero",
+preco: "$4.00",
+Img: "assets/foto Pastel de Chocolate.jpg"
+    },
+
+{
+nome: "Croissant",
+descricao: "Croissant francés recién horneado",
+preco: "$3.00",
+img: "assets/foto Croissant.jpg"
+},
+ 
+{
+nome: "Muffin con Arándanos",
+descricao: "Esponjoso muffin con arándanos frescos",
+preco: "$3.50",
+img: "assets/foto muffin.jpg"
+}
+]
+
+const comida = [
+    {
+nome: "Sándwich de Pavo",
+descricao: "Sándwich fresco con pavo y vegetales",
+preco: "$6.50",
+img: "assets/foto sanduiche.jpg"
+}
+]
